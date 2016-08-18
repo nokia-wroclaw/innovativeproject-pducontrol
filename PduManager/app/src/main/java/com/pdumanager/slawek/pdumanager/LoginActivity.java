b96f3e74b7dcab7db19e300bb1adef0236883a74
slawomirzaba <slawekzaba@poczta.fm>
2016-08-18 14:11:57 +0200

    display devices - jsonParser + arrayAdapters

--- PduManager/app/src/main/java/com/pdumanager/slawek/pdumanager/LoginActivity.java
+++ PduManager/app/src/main/java/com/pdumanager/slawek/pdumanager/LoginActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private Button mLoginButton;
