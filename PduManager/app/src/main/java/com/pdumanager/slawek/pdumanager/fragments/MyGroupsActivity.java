40650db0212eb76151510846f4a434f685f09c69
slawomirzaba <slawekzaba@poczta.fm>
2016-08-19 11:20:03 +0200

    private groups

--- PduManager/app/src/main/java/com/pdumanager/slawek/pdumanager/fragments/MyGroupsActivity.java
+++ PduManager/app/src/main/java/com/pdumanager/slawek/pdumanager/fragments/MyGroupsActivity.java
@@ -7,14 +7,71 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.pdumanager.slawek.pdumanager.R;
+import com.pdumanager.slawek.pdumanager.arrayAdapters.DeviceArrayAdapter;
+import com.pdumanager.slawek.pdumanager.arrayAdapters.GroupArrayAdapet;
+import com.pdumanager.slawek.pdumanager.model.DeviceResponse;
+import com.pdumanager.slawek.pdumanager.model.GroupResponse;
 
-public class MyGroupsActivity extends Fragment {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyGroupsActivity extends Fragment implements AdapterView.OnItemClickListener {
+    private ListView mGroupsListView;
+    private GroupArrayAdapet mGroupsAdapter;
+    private GroupResponse mResponse;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_my_groups, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.activity_my_groups, container, false);
+        mGroupsAdapter = new GroupArrayAdapet(this.getActivity(), R.layout.private_group_on_list);
+        mGroupsListView = (ListView) view.findViewById(R.id.private_groups_list);
+        mGroupsListView.setAdapter(mGroupsAdapter);
+        mGroupsListView.setOnItemClickListener(this);
+        try {
+            JSONObject jsonObject = new JSONObject(readTextFromRawResource(R.raw.groups));
+            mResponse = GroupResponse.fromJsonObject(jsonObject);
+            fillListWithGroups();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return view;
+    }
+
+    private void fillListWithGroups() {
+        mGroupsAdapter.setGroups(mResponse.groups);
+    }
+
+
+    private String readTextFromRawResource(int resourceId) {
+        InputStream input = getResources().openRawResource(resourceId);
+        ByteArrayOutputStream byteArrayOutputStream= new ByteArrayOutputStream();
+        int i;
+        try{
+            i = input.read();
+            while(i != -1){
+                byteArrayOutputStream.write(i);
+                i = input.read();
+            }
+            input.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return byteArrayOutputStream.toString();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
     }
 }
---
+++ PduManager/app/src/main/java/com/pdumanager/slawek/pdumanager/model/Group.java
@@ -0,0 +1,12 @@
+package com.pdumanager.slawek.pdumanager.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by slawek on 19.08.16.
+ */
+public class Group implements Serializable {
+    public int id;
+    public String name;
+    public String owner;
+}
