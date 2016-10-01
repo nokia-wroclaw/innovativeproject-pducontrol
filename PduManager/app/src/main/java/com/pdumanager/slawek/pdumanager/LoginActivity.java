2b7e0932619aeb6144d09cc7c78eb6ea617c9639
mat183 <mateow1994@gmail.com>
2016-10-01 13:41:32 +0200

    login by rest

--- PduManager/app/src/main/java/com/pdumanager/slawek/pdumanager/LoginActivity.java
+++ PduManager/app/src/main/java/com/pdumanager/slawek/pdumanager/LoginActivity.java
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.icu.text.SimpleDateFormat;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,33 +12,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.unboundid.ldap.sdk.BindRequest;
-import com.unboundid.ldap.sdk.BindResult;
-import com.unboundid.ldap.sdk.LDAPConnection;
-import com.unboundid.ldap.sdk.LDAPException;
-import com.unboundid.ldap.sdk.SimpleBindRequest;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
-import java.sql.Date;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private Button mLoginButton;
     private EditText mUsernameEditText;
     private EditText mPasswordEditText;
 
-    private boolean loginFlag = true;
     private boolean backPressedToExit;
 
     public static final String Username = "username";
     public static final String Password = "password";
-
-    String password = "secret";
-    String address = "ldaps://ed-p-gl.emea.nsn-net.net";
-    String bindDN = "uid=%(user)s,ou=users,dc=example,dc=com";
-    int port = 389;
-
-    LDAPConnection connection;
-    BindRequest bindRequest;
-    BindResult bindResult;
 
     SharedPreferences sharedPrefs;
     SharedPreferences.Editor editor;
@@ -76,49 +68,56 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         editor = sharedPrefs.edit();
 
-//        try {
-//            connection = new LDAPConnection(address, port, bindDN, password);
-//            connection.setConnectionName("LDAPConnection");
-//            bindRequest = new SimpleBindRequest(username, password);
-//            bindResult = connection.bind(bindRequest);
-//
-//            if(bindResult.getResultCode().isConnectionUsable()) {
-//                String conName = connection.getConnectionName();
-//                long time = connection.getConnectTime();
-//                java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat("dd-MM-yy HH:mm:ss");
-//                String dateString = formatter.format(new Date(time));
-//                Toast.makeText(getBaseContext(),
-//                               "Connected to LDAP server....connection_name="+conName+" at time"+dateString,
-//                               Toast.LENGTH_LONG).show();
-//            } else {
-//                Toast.makeText(this, "Wrong credentials!", Toast.LENGTH_LONG).show();
-//            }
-//        } catch(LDAPException e) {
-//            loginFlag = false;
-//            e.printStackTrace();
-//            Toast.makeText(getBaseContext(),"No connection was established" , Toast.LENGTH_LONG).show();
-//        } catch(Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            if(loginFlag) {
-//                connection.close();
-//                Toast.makeText(getBaseContext(), "Connection Closed successfully", Toast.LENGTH_LONG).show();
-//            }
-//        }
+        try {
+            String response = new LoginAttempt().execute(username, password).get();
 
-        if(username.equals("mwojdyla") && password.equals("zaq12wsx"))
-        {
-            editor.putString(Username, username);
-            editor.putString(Password, password);
-            editor.commit();
+            if(response != null && response.equals("true"))
+            {
+                editor.putString(Username, username);
+                editor.putString(Password, password);
+                editor.commit();
 
-            Toast.makeText(this, "Logged: " + username, Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(this, MenuActivity.class);
-            startActivity(intent);
+                Toast.makeText(this, "Logged: " + username, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, MenuActivity.class);
+                startActivity(intent);
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(), "Wrong credentials!", Toast.LENGTH_LONG).show();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
-        else
-        {
-            Toast.makeText(getApplicationContext(), "Wrong credentials!", Toast.LENGTH_LONG).show();
+
+    }
+
+    class LoginAttempt extends AsyncTask<String, Object, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String result = null;
+            String username = params[0];
+            String password = params[1];
+            String modifiedURL = Constants.LOGIN_URL + "username=" + username + "&password=" + password;
+
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet(modifiedURL);
+
+            try {
+                HttpResponse httpResponse = httpClient.execute(httpGet);
+                HttpEntity httpEntity = httpResponse.getEntity();
+
+                if(httpEntity != null && httpResponse.getStatusLine().getStatusCode() == 200) {
+                    result = EntityUtils.toString(httpEntity);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return result;
         }
     }
+
 }
