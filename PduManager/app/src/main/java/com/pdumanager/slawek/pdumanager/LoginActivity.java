87120e3b143f58ad8b335fcd9f2e0d7135fa8497
mat183 <mateow1994@gmail.com>
2016-10-10 23:55:38 +0200

    recognise if app has access to network

--- PduManager/app/src/main/java/com/pdumanager/slawek/pdumanager/LoginActivity.java
+++ PduManager/app/src/main/java/com/pdumanager/slawek/pdumanager/LoginActivity.java
@@ -4,9 +4,12 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,8 +19,13 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.HttpHostConnectException;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+//import org.spongycastle.crypto.PBEParametersGenerator;
+//import org.spongycastle.crypto.digests.SHA256Digest;
+//import org.spongycastle.crypto.generators.PKCS5S2ParametersGenerator;
+//import org.spongycastle.crypto.params.KeyParameter;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
@@ -45,7 +53,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mPasswordEditText = (EditText) findViewById(R.id.password_input);
 
         sharedPrefs = getSharedPreferences(MenuActivity.MyPREFERENCES, Context.MODE_PRIVATE);
-
+        if(!isConnected()) {
+            Toast.makeText(this, "Connection is not available. Please check your network and restart app.", Toast.LENGTH_LONG).show();
+        }
         mLoginButton.setOnClickListener(this);
     }
 
@@ -71,7 +81,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         try {
             String response = new LoginAttempt().execute(username, password).get();
 
-            if(response != null && response.equals("true"))
+            if(response == null)
+            {
+                Toast.makeText(this, "Unknown error, please try again...", Toast.LENGTH_LONG).show();
+            }
+            else if(response != null && response.equals("true"))
             {
                 editor.putString(Username, username);
                 editor.putString(Password, password);
@@ -81,9 +95,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 Intent intent = new Intent(this, MenuActivity.class);
                 startActivity(intent);
             }
-            else
+            else if(response != null && response.equals("false"))
             {
-                Toast.makeText(getApplicationContext(), "Wrong credentials!", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Wrong credentials!", Toast.LENGTH_LONG).show();
+            }
+            else if(response != null && response.equals("refused"))
+            {
+                Toast.makeText(this, "Connection to server refused.", Toast.LENGTH_LONG).show();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -93,6 +111,15 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+
     class LoginAttempt extends AsyncTask<String, Object, String> {
 
         @Override
@@ -100,6 +127,21 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             String result = null;
             String username = params[0];
             String password = params[1];
+
+//            //1
+//            PKCS5S2ParametersGenerator gen = new PKCS5S2ParametersGenerator(new SHA256Digest());
+//            try {
+//                gen.init("password".getBytes("UTF-8"), "salt".getBytes(), 4096);
+//            } catch (UnsupportedEncodingException e) {
+//                e.printStackTrace();
+//            }
+//            byte[] dk = ((KeyParameter) gen.generateDerivedParameters(256)).getKey();
+//
+//            //2
+//            PKCS5S2ParametersGenerator generator = new PKCS5S2ParametersGenerator(new SHA256Digest());
+//            generator.init(PBEParametersGenerator.PKCS5PasswordToUTF8Bytes((char[])password), salt, iterations);
+//            KeyParameter key = (KeyParameter)generator.generateDerivedMacParameters(keySizeInBits);
+
             String modifiedURL = Constants.LOGIN_URL + "username=" + username + "&password=" + password;
 
             HttpClient httpClient = new DefaultHttpClient();
@@ -109,15 +151,22 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 HttpResponse httpResponse = httpClient.execute(httpGet);
                 HttpEntity httpEntity = httpResponse.getEntity();
 
-                if(httpEntity != null && httpResponse.getStatusLine().getStatusCode() == 200) {
+                if (httpEntity != null && httpResponse.getStatusLine().getStatusCode() == 200) {
                     result = EntityUtils.toString(httpEntity);
                 }
+            } catch(HttpHostConnectException e) {
+                Log.w("doInBackground", "Connection to server refused");
+                result = "refused";
+                return result;
             } catch (IOException e) {
+                Log.e("doInBackground", "Caught IOException");
                 e.printStackTrace();
+                return result;
             }
 
             return result;
         }
+
     }
 
 }
