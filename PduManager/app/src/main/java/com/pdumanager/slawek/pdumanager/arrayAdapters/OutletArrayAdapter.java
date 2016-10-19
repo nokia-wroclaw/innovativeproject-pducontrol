baf2cf40aa51de1eeefed7676a8f9f873325e144
mat183 <mateow1994@gmail.com>
2016-10-19 16:54:03 +0200

    management devices by buttons

--- PduManager/app/src/main/java/com/pdumanager/slawek/pdumanager/arrayAdapters/OutletArrayAdapter.java
+++ PduManager/app/src/main/java/com/pdumanager/slawek/pdumanager/arrayAdapters/OutletArrayAdapter.java
@@ -1,18 +1,34 @@
 package com.pdumanager.slawek.pdumanager.arrayAdapters;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.pdumanager.slawek.pdumanager.Constants;
 import com.pdumanager.slawek.pdumanager.R;
 import com.pdumanager.slawek.pdumanager.model.Device;
 import com.pdumanager.slawek.pdumanager.model.Outlet;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ConnectTimeoutException;
+import org.apache.http.conn.HttpHostConnectException;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
+import java.io.IOException;
 
 /**
  * Created by slawek on 24.08.16.
@@ -20,11 +36,13 @@ import com.pdumanager.slawek.pdumanager.model.Outlet;
 public class OutletArrayAdapter extends ArrayAdapter<Outlet> {
     private final LayoutInflater mInflater;
     private final int mResource;
+    private final String ipNumber;
 
-    public OutletArrayAdapter(Context context, int resource) {
+    public OutletArrayAdapter(Context context, int resource, String ip) {
         super(context, R.layout.outlet_on_list);
         mResource = resource;
         mInflater = LayoutInflater.from(context);
+        ipNumber = ip;
     }
 
     public void setOutlets(Outlet[] outlets){
@@ -63,6 +81,10 @@ public class OutletArrayAdapter extends ArrayAdapter<Outlet> {
             viewHolder.mOutletDescription = (TextView) convertView.findViewById(R.id.outlet_description);
             viewHolder.mOutletLineraLayout = (LinearLayout) convertView.findViewById(R.id.outlet_on_list);
             viewHolder.outletState = (ImageView) convertView.findViewById((R.id.outlet_state));
+            viewHolder.onButton = (Button) convertView.findViewById(R.id.button);
+            viewHolder.offButton = (Button) convertView.findViewById(R.id.button2);
+            viewHolder.resetButton = (Button) convertView.findViewById(R.id.button3);
+            viewHolder.checkButton = (Button) convertView.findViewById(R.id.button4);
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
@@ -70,6 +92,13 @@ public class OutletArrayAdapter extends ArrayAdapter<Outlet> {
         Outlet outlet = getItem(position);
         viewHolder.outletTextView.setText("outlet: " + outlet.number);
         viewHolder.mOutletDescription.setText(outlet.description);
+
+        View.OnClickListener buttonListener = new ButtonClickListener(convertView, ipNumber, outlet.number);
+
+        viewHolder.onButton.setOnClickListener(buttonListener);
+        viewHolder.offButton.setOnClickListener(buttonListener);
+        viewHolder.resetButton.setOnClickListener(buttonListener);
+        viewHolder.checkButton.setOnClickListener(buttonListener);
 
         viewHolder.outletState.setImageResource(R.drawable.grey_circle);
         return convertView;
@@ -80,5 +109,67 @@ public class OutletArrayAdapter extends ArrayAdapter<Outlet> {
         TextView outletTextView;
         TextView mOutletDescription;
         ImageView outletState;
+        Button onButton;
+        Button offButton;
+        Button resetButton;
+        Button checkButton;
+    }
+
+    public class ButtonClickListener implements View.OnClickListener
+    {
+        private View buttonView;
+        private String pduIp;
+        private int outletNumber;
+
+        public ButtonClickListener(View v, String ip, int number)
+        {
+            buttonView = v;
+            pduIp = ip;
+            outletNumber = number;
+        }
+
+        @Override
+        public void onClick(View v) {
+            String url = "";
+            HttpParams httpParams = new BasicHttpParams();
+            HttpConnectionParams.setConnectionTimeout(httpParams, 3000);
+            HttpConnectionParams.setSoTimeout(httpParams, 5000);
+            HttpClient httpClient = new DefaultHttpClient(httpParams);
+
+            switch(v.getId())
+            {
+                case R.id.button:
+                    url = Constants.SWITCH_ON + "?outlet_nr=" + outletNumber + "&pdu_ip=" + pduIp;
+                    Log.i("OutletArrayAdapter", "Clicked ON! " + url);
+                    break;
+                case R.id.button2:
+                    url = Constants.SWITCH_OFF + "?outlet_nr=" + outletNumber + "&pdu_ip=" + pduIp;
+                    Log.i("OutletArrayAdapter", "Clicked OFF! " + url);
+                    break;
+                case R.id.button3:
+                    url = Constants.RESET + "?outlet_nr=" + outletNumber + "&pdu_ip=" + pduIp;
+                    Log.i("OutletArrayAdapter", "Clicked RESET! " + url);
+                    break;
+                case R.id.button4:
+                    url = Constants.CHECK_STATE + "?outlet_nr=" + outletNumber + "&pdu_ip=" + pduIp;
+                    Log.i("OutletArrayAdapter", "Clicked CHECK! " + url);
+                    break;
+                default:
+                    Log.w("OutletArrayAdapter", "Unknown button ID, url is empty!");
+                    break;
+            }
+
+            HttpGet httpGet = new HttpGet(url);
+
+            try {
+                HttpResponse httpResponse = httpClient.execute(httpGet);
+            } catch(HttpHostConnectException e) {
+                Log.w("doInBackground", "Connection to server refused");
+            } catch (ConnectTimeoutException e) {
+                Log.w("doInBackground", "Connection timed out");
+            } catch (IOException e) {
+                Log.e("doInBackground", "Caught IOException");
+            }
+        }
     }
 }
