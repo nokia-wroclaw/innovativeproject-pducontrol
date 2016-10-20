51e1830da6ca9c7775a87e501cfe95be2846bcf5
slawek <slawekzaba@poczta.fm>
2016-10-20 20:31:41 +0200

    devices management

--- PduManager/app/src/main/java/com/pdumanager/slawek/pdumanager/Constants.java
+++ PduManager/app/src/main/java/com/pdumanager/slawek/pdumanager/Constants.java
@@ -8,10 +8,10 @@ public class Constants {
         //praca
         //public static final String PDU_MANAGER_URL = "http://192.168.5.116:8000";
         //dom
-        //public static final String PDU_MANAGER_URL = "http://192.168.8.101:8000";
+        public static final String PDU_MANAGER_URL = "http://192.168.8.100:8000";
 
         //url Matiego
-        public static final String PDU_MANAGER_URL = "http://192.168.1.30:3500";
+        //public static final String PDU_MANAGER_URL = "http://192.168.1.30:3500";
 
         //url Kacpra
         //public static final String PDU_MANAGER_URL = "http://192.168.0.101:8000";
@@ -24,8 +24,8 @@ public class Constants {
         public static final String DEVICES_URL = PDU_MANAGER_URL + "/api/pdus/";
         public static final String OUTLETS_URL = PDU_MANAGER_URL + "/api/pdu_outlets/";
         public static final String IMPORT_GROUP_URL = PDU_MANAGER_URL + "/api/group/mobile_edit_user_groups/?username=";
-        public static final String SWITCH_ON = PDU_MANAGER_URL + "/pdu_communicator/switch_outlet_on";
-        public static final String SWITCH_OFF = PDU_MANAGER_URL + "/pdu_communicator/switch_outlet_off";
-        public static final String RESET = PDU_MANAGER_URL + "/pdu_communicator/reset_outlet";
-        public static final String CHECK_STATE = PDU_MANAGER_URL + "/pdu_communicator/check_state";
+        public static final String SWITCH_ON = PDU_MANAGER_URL + "/pdu_communicator/switch_outlet_on/";
+        public static final String SWITCH_OFF = PDU_MANAGER_URL + "/pdu_communicator/switch_outlet_off/";
+        public static final String RESET = PDU_MANAGER_URL + "/pdu_communicator/reset_outlet/";
+        public static final String CHECK_STATE = PDU_MANAGER_URL + "/pdu_communicator/check_state/";
 }
